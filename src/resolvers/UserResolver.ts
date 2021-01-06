24c64979de3f2322f1dfef0af3b464ea85aa536b
pikokr <parangee9706@gmail.com>
2021-01-06 19:46:38 +0900

    feat: add admin field on model User

--- src/resolvers/UserResolver.ts
+++ src/resolvers/UserResolver.ts
@@ -1,6 +1,26 @@
-import { Resolver } from "type-graphql";
+import { FieldResolver, Resolver, Root } from "type-graphql";
 import User from "../types/User";
+import Util from "../Util";
 
 @Resolver(User)
 export default class {
+    @FieldResolver()
+    async tag(@Root() parent: User) {
+        const data = await Util.prisma.user.findFirst({
+            where: {
+                id: parent.id
+            }
+        })
+        return data.username + '#' + data.discriminator
+    }
+
+    @FieldResolver()
+    async avatarURL(@Root() parent: User) {
+        const data = await Util.prisma.user.findFirst({
+            where: {
+                id: parent.id
+            }
+        })
+        return data.avatar ? `https://cdn.discordapp.com/avatars/${data.id}/${data.avatar}` : `https://cdn.discordapp.com/embed/avatars/${Number(data.discriminator) % 5}`
+    }
 }
