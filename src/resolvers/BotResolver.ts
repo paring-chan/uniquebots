dfad3c6ebd5cf1006437bf8c105422d96c72a0f1
pikokr <parangee9706@gmail.com>
2021-01-08 17:24:18 +0900

    feat: add bot

--- src/resolvers/BotResolver.ts
+++ src/resolvers/BotResolver.ts
@@ -8,28 +8,56 @@ export default class {
   async addBot(
     @Ctx() ctx,
     @Arg('id') id: string,
-    @Arg('category', (type) => [String], { validate: false }) category: string[],
+    @Arg('category', (type) => [String], { validate: false })
+    category: string[],
     @Arg('brief') brief: string,
     @Arg('description') description: string,
-    @Arg('library', {nullable: true}) library: string,
-    @Arg('website', {nullable: true}) website: URL,
-    @Arg('git', {nullable: true}) git: URL
+    @Arg('library', { nullable: true }) library: string,
+    @Arg('website', { nullable: true }) website: URL,
+    @Arg('git', { nullable: true }) git: URL,
+    @Arg('prefix') prefix: string,
+    @Arg('support', { nullable: true }) support: URL,
   ) {
-    if (!ctx.user) throw new ApolloError('Login is required to add bot', 'ERR_LOGIN_REQUIRED')
+    if (!ctx.user)
+      throw new ApolloError(
+        'Login is required to add bot',
+        'ERR_LOGIN_REQUIRED',
+      )
 
-    if (await Util.prisma.bot.findUnique({
-      where: {
-        id
-      }
-    })) throw new ApolloError('Bot already exists.', 'ERR_BOT_ALREADY_EXISTS')
+    if (!id) throw new ApolloError('ID is required.', 'VALIDATION_ERROR')
+    if (!brief) throw new ApolloError('Brief is required.', 'VALIDATION_ERROR')
+    if (!description)
+      throw new ApolloError('Description is required.', 'VALIDATION_ERROR')
 
-    if (brief.length > 50) throw new ApolloError('Max length of brief is 50 characters.', 'VALIDATION_ERRROR')
-    if (description.length > 5000) throw new ApolloError('Max length of description is 5000 characters.', 'VALIDATION_ERRROR')
+    if (
+      await Util.prisma.bot.findUnique({
+        where: {
+          id,
+        },
+      })
+    )
+      throw new ApolloError('Bot already exists.', 'ERR_BOT_ALREADY_EXISTS')
+
+    if (brief.length > 50)
+      throw new ApolloError(
+        'Max length of brief is 50 characters.',
+        'VALIDATION_ERRROR',
+      )
+    if (description.length > 5000)
+      throw new ApolloError(
+        'Max length of description is 5000 characters.',
+        'VALIDATION_ERRROR',
+      )
+    if (prefix.length > 10)
+      throw new ApolloError(
+        'Max length of prefix is 10 characters.',
+        'VALIDATION_ERRROR',
+      )
 
     const lib = await Util.prisma.library.findUnique({
       where: {
-        id: library
-      }
+        id: library,
+      },
     })
 
     if (!lib) throw new ApolloError('Library Not Found', 'VALIDATION_ERROR')
@@ -43,6 +71,31 @@ export default class {
       if (!categories.find((r) => r.id === c))
         throw new ApolloError(`Category '${c}' not found`, 'VALIDATION_ERROR')
     }
+    await Util.prisma.bot.create({
+      data: {
+        id,
+        prefix,
+        avatarURL: 'https://cdn.discordapp.com/embed/avatars/0.png',
+        library: {
+          connect: {
+            id: lib.id,
+          },
+        },
+        username: '',
+        git: git as string,
+        website: website as string,
+        support: support as string,
+        owner: {
+          connect: {
+            id: ctx.user.id,
+          },
+        },
+        categories: {
+          connect: category.map((r) => ({ id: r })),
+        },
+      },
+    })
+
     return true
   }
 }
