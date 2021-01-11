61f14ec079b209097cdfaa7c6e50f2d242d4618b
piko <parangee9706@gmail.com>
2021-01-11 13:12:47 +0900

    ✨ simple pagination

--- pages/categories/[id].tsx
+++ pages/categories/[id].tsx
@@ -4,8 +4,13 @@ import { getApolloClient } from '../../lib/apollo'
 import { gql } from 'apollo-boost'
 import { Category } from '../../types'
 import { NextSeo } from 'next-seo'
+import BotCard from '../../components/BotCard'
+import clsx from 'clsx'
 
 const CategoryPage: NextPage<{ category: Category }> = ({ category }) => {
+  const [currentPage, setCurrentPage] = React.useState(3)
+  const [pages] = React.useState(4)
+
   return (
     <div>
       <NextSeo
@@ -13,6 +18,49 @@ const CategoryPage: NextPage<{ category: Category }> = ({ category }) => {
         description={`카테고리 ${category.name}의 봇 목록입니다.`}
       />
       <div className="text-3xl">카테고리 - {category.name}</div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-2 grid-cols-1">
+        {category.bots.map((it, key) => (
+          <BotCard bot={it} key={key} />
+        ))}
+      </div>
+      <div className="flex justify-center gap-1">
+        {(() => {
+          const items = new Array(pages).fill(null).map((it, idx) => idx + 1)
+          return items
+            .slice(
+              pages <= 3
+                ? 0
+                : currentPage === 1
+                ? 0
+                : currentPage === pages
+                ? currentPage - 3
+                : currentPage - 2,
+              pages <= 3
+                ? items.length
+                : currentPage === 1
+                ? currentPage + 2
+                : currentPage === pages
+                ? currentPage
+                : currentPage + 1,
+            )
+            .map((it, key) => (
+              <div
+                key={key}
+                className={clsx(
+                  'p-2 rounded-full w-8 h-8 flex justify-center cursor-pointer transition-colors',
+                  {
+                    'bg-blue-500': currentPage === it,
+                    'bg-discord-black': currentPage !== it,
+                  },
+                )}
+                style={{ alignItems: 'center' }}
+                onClick={() => setCurrentPage(it)}
+              >
+                {it}
+              </div>
+            ))
+        })()}
+      </div>
     </div>
   )
 }
