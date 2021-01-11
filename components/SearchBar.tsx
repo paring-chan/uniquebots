1befb445e4bf667b600133eb63089e2831fa13ae
piko <parangee9706@gmail.com>
2021-01-11 22:03:32 +0900

    :bug: search bar width

--- components/SearchBar.tsx
+++ components/SearchBar.tsx
@@ -6,30 +6,28 @@ const SearchBar = ({ initial }: { initial?: string }) => {
   const [keyword, setKeyword] = React.useState(initial || '')
 
   return (
-    <div className="flex justify-center">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          if (!keyword) return
-          return Router.push(
-            '/search/[id]',
-            '/search/' + encodeURIComponent(keyword),
-          )
-        }}
-        className="dark:bg-discord-black border-2 border-discord-black rounded-md flex max-w-screen-lg w-full"
-      >
-        <input
-          onChange={(e) => setKeyword(e.target.value)}
-          value={keyword}
-          type="text"
-          placeholder="봇 검색하기"
-          className="bg-transparent p-3 outline-none flex-grow min-w-0"
-        />
-        <button type="submit" className="p-3 bg-transparent">
-          <FontAwesomeIcon size="2x" icon={['fas', 'search']} />
-        </button>
-      </form>
-    </div>
+    <form
+      onSubmit={(e) => {
+        e.preventDefault()
+        if (!keyword) return
+        return Router.push(
+          '/search/[id]',
+          '/search/' + encodeURIComponent(keyword),
+        )
+      }}
+      className="dark:bg-discord-black border-2 border-discord-black rounded-md flex w-full"
+    >
+      <input
+        onChange={(e) => setKeyword(e.target.value)}
+        value={keyword}
+        type="text"
+        placeholder="봇 검색하기"
+        className="bg-transparent p-3 outline-none flex-grow min-w-0"
+      />
+      <button type="submit" className="p-3 bg-transparent">
+        <FontAwesomeIcon size="2x" icon={['fas', 'search']} />
+      </button>
+    </form>
   )
 }
 
