8a78a5919d0885b1216507aff34541e3cac03ce5
piko <parangee9706@gmail.com>
2021-01-29 17:28:41 +0900

    feat: adfit

--- components/Advertisement.tsx
+++ components/Advertisement.tsx
@@ -5,29 +5,37 @@ const googleAdId = 'ca-pub-4808458196722819'
 const slot = '1227211759'
 
 class Advertisement extends Component<{ className?: string }> {
-  googleInit: any = null
+  // googleInit: any = null
 
   componentDidMount() {
-    this.googleInit = setTimeout(() => {
-      if (typeof window !== 'undefined')
-        // @ts-ignore
-        (window.adsbygoogle || []).push({})
-    }, 1000)
+    // this.googleInit = setTimeout(() => {
+    //   if (typeof window !== 'undefined')
+    //     // @ts-ignore
+    //     (window.adsbygoogle || []).push({})
+    // }, 1000)
+    ;(window as any).adfit?.()
   }
   componentWillUnmount() {
-    if (this.googleInit) clearTimeout(this.googleInit)
+    // if (this.googleInit) clearTimeout(this.googleInit)
   }
 
   render() {
     return (
       <div className={this.props.className}>
-        <ins
+        {/* <ins
           className="adsbygoogle"
           style={{ display: 'block' }}
           data-ad-client={googleAdId}
           data-ad-slot={slot}
           data-ad-format="auto"
           data-full-width-responsive="true"
+        ></ins> */}
+        <ins
+          className="kakao_ad_area"
+          style={{ display: 'none' }}
+          data-ad-unit="DAN-cg3Y6eju0wg9GWlA"
+          data-ad-width="320"
+          data-ad-height="100"
         ></ins>
       </div>
     )
