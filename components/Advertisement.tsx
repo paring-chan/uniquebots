c2db03c7f778b0f2c0b71aeddb701dca17d07abf
UniqueDevStorm <storm@uniquecode.team>
2021-03-09 10:23:09 +0900

    refactor: change ad

--- components/Advertisement.tsx
+++ components/Advertisement.tsx
@@ -1,35 +1,23 @@
-import React, { Component } from 'react'
-
-const googleAdId = 'ca-pub-4808458196722819'
-
-const slot = '1227211759'
+import React, { Component } from 'react';
 
 class Advertisement extends Component<{ className?: string }> {
-  googleInit: any = null
-
   componentDidMount() {
-    this.googleInit = setTimeout(() => {
-      if (typeof window !== 'undefined')
-        // @ts-ignore
-        (window.adsbygoogle || []).push({})
-    }, 1000)
+    ;(window as any).adfit?.()
   }
   componentWillUnmount() {
-    if (this.googleInit) clearTimeout(this.googleInit)
   }
 
   render() {
     return (
-      <div className={this.props.className}>
-        <ins
-          className="adsbygoogle"
-          style={{ display: 'block' }}
-          data-ad-client={googleAdId}
-          data-ad-slot={slot}
-          data-ad-format="auto"
-          data-full-width-responsive="true"
-        />
-      </div>
+        <div className={this.props.className}>
+          <ins
+              className="kakao_ad_area"
+              style={{ display: 'none' }}
+              data-ad-unit="DAN-cg3Y6eju0wg9GWlA"
+              data-ad-width="320"
+              data-ad-height="100"
+          />
+        </div>
     )
   }
 }
